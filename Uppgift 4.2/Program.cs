7667258adf80f4ffa06ae308d36180b6436a9fad
sere06001 <sere06001@utb.vaxjo.se>
2023-10-20 16:26:05 +0200

    Add project files.

---
+++ Uppgift 4.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Uppgift4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            int tal1 = int.Parse(Console.ReadLine());
+            int störstaTal = tal1;
+
+            Console.WriteLine("Vill du skriva in ett till heltal? Skriv j för ja och n för nej.");
+            string svar = Console.ReadLine();
+
+            switch (svar)
+            {
+                case "j":
+                    string svar1 = "";
+                    do
+                    {
+                        Console.WriteLine("Skriv in ditt nya tal.");
+                        int nyaTal = int.Parse(Console.ReadLine());
+                        if (nyaTal > störstaTal)
+                        {
+                            störstaTal = nyaTal;
+                        }
+                        Console.WriteLine("Vill du skriva in ett till heltal?");
+                        string svar2 = Console.ReadLine();
+                        svar1 = svar2;
+                    }
+                    while (svar1 == "j");
+
+                    Console.WriteLine($"{störstaTal} är det största talet du angett.");
+                    break;
+
+                case "n":
+                    Console.WriteLine($"{störstaTal} är det största talet du angett");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
